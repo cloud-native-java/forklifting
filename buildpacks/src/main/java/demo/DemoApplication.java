2d7f4194d145ef27314baed61128613509b83039
Josh Long <josh@joshlong.com>
2015-11-24 23:33:47 -0800

    updated

--- buildpacks/src/main/java/demo/DemoApplication.java
+++ buildpacks/src/main/java/demo/DemoApplication.java
@@ -7,19 +7,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
 @WebServlet("/hi")
 public class DemoApplication extends HttpServlet {
 
-    protected void doGet(
-            HttpServletRequest request,
-            HttpServletResponse response)
-            throws ServletException, IOException {
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
 
         response.setContentType("text/html");
 
         response.getWriter()
-            .print("<html><body><h3>Hello Cloud</h3></body></html>");
+                .print("<html><body><h3>Hello Cloud</h3></body></html>");
     }
 }
